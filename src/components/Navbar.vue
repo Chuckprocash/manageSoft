b7e17197e6ff9ee5c8c6f2fa6add19799405fe44
chuckprocash <e.procashchakraborty@gmail.com>
2025-04-21 19:32:28 +0600

    initial commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,15 @@
-<script setup></script>
+<script setup>
+    import { ref } from 'vue';
+    const menu = ref(null);
+    const close = ref(null);
+    const open = ref(null);
+    const toggleMenu = () => {
+        menu.value.classList.toggle('hidden');
+        close.value.classList.toggle('hidden');
+        open.value.classList.toggle('hidden');
+    };
+
+</script>
 <template>
     <section>
         <nav class="container relative mx-auto p-6">
@@ -17,7 +28,23 @@
                 <a href="#" class="p-2 px-6 mt-3 text-white bg-orange-700 baseline rounded-full hidden md:block hover:bg-orange-600 transition duration-200">
                     Get Started
                 </a>
+
+                <button id="menu-btn" class="block hamburger md:hidden cursor-pointer focus:outline-none" @click="toggleMenu">
+                    <img src="/images/hamburger-menu-svgrepo-com.svg" alt="" class="h-8" ref="open" />
+                    <img src="/images/cross-svgrepo-com.svg" alt="" class="h-8 hidden" ref="close" />
+                </button>
             </div>
+            <div class="md:hidden">
+                <div ref="menu" class="absolute left-6 right-6 flex flex-col items-center hidden self-end py-8 mt-10 space-y-6 font-bold
+                 bg-white sm:w-auto sm:self-center shadow-md">
+                    <a class="hover:text-gray-600 " href="#">Product</a>
+                    <a class="hover:text-gray-600" href="#">Pricing</a>
+                    <a class="hover:text-gray-600" href="#">Careers</a>
+                    <a class="hover:text-gray-600" href="#">Community</a>
+                    <a class="hover:text-gray-600" href="#">About Us</a>
+                </div>
+            </div>
+
         </nav>
     </section>
 </template>
@@ -33,5 +60,12 @@
 }
 nav a{
     font-weight: 600;
+}
+.hamburger{
+    position: relative;
+    cursor: pointer;
+    height: 24px;
+    width: 24px;
+    transition: all 0.3s ease-in-out;
 }
 </style>
